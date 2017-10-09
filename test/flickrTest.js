0b4effb45dbb2d6eafc6fbe1a3022fa298774caa
Georg <davedelorean123@gmail.com>
2017-10-08 18:36:03 -0700

    added additional tests

--- test/flickrTest.js
+++ test/flickrTest.js
@@ -7,10 +7,9 @@ let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('../app');
 let should = chai.should();
-
 chai.use(chaiHttp);
 
-/*
+  /*
   * Test the image search
   */
   describe('send image search request', () => {
@@ -18,8 +17,39 @@ chai.use(chaiHttp);
         chai.request(server)
             .get('/images?query=apples')
             .end((err, res) => {
+                res.should.have.status(200);
                 res.body.should.be.a('array');
+              done();
+            });
+      });
+  });
 
+  /*
+  * Test wrong user input
+  */
+  describe('send image search request with empty searchPhrase', () => {
+      it('it should return a promt how to use the api correctly', (done) => {
+        chai.request(server)
+            .get('/images?query=')
+            .end((err, res) => {
+              res.should.have.status(200);
+              res.text.should.equal('usage: /images?query=yourSearchWordHere')
+              done();
+            });
+      });
+  });
+
+
+  /*
+  * Test wrong route
+  */
+  describe('send image search request with empty searchPhrase', () => {
+      it('it should return a promt how to use the api correctly', (done) => {
+        chai.request(server)
+            .get('/bla')
+            .end((err, res) => {
+              res.should.have.status(200);
+              res.text.should.equal('usage: /images?query=yourSearchWordHere')
               done();
             });
       });
